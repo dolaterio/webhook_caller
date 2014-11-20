6620b3dc3b0e70a69e2b3d8c052e1ffa920d33b1
Albert Callarisa <shark234@gmail.com>
2014-11-20 17:29:29 +0100

    More generic requests image

--- run.py
+++ run.py
@@ -10,25 +10,7 @@ if not 'url' in obj:
   sys.exit(1)
 
 url = obj["url"]
-
-values = {}
-
-if "status" in obj:
-  values["status"] = obj["status"]
-if "stdout" in obj:
-  values["stdout"] = obj["stdout"]
-if "stderr" in obj:
-  values["stderr"] = obj["stderr"]
-if "error" in obj:
-  values["error"] = obj["error"]
-if "job_id" in obj:
-  values["job_id"] = obj["job_id"]
-if "image_id" in obj:
-  values["image_id"] = obj["image_id"]
-if "duration" in obj:
-  values["duration"] = obj["duration"]
-if "created_at" in obj:
-  values["created_at"] = obj["created_at"]
+values = obj["values"]
 
 data = urllib.parse.urlencode(values)
 data = data.encode('utf-8')
